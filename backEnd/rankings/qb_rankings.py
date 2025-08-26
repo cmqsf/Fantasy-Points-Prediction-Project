5147dedeb909a6ffcbbbb233dc2ca63b010d791a
cmqsf <cmquarterman@gmail.com>
2025-08-25 19:30:22 -0700

    Change db column names

--- backEnd/rankings/qb_rankings.py
+++ backEnd/rankings/qb_rankings.py
@@ -9,7 +9,8 @@ import logging
 import statistics
 
 from fastapi import FastAPI, HTTPException, APIRouter
-from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 
 from fastapi.middleware.cors import CORSMiddleware
@@ -19,12 +20,15 @@ from contextlib import asynccontextmanager
 async def router_lifespan(app: APIRouter):
     global stats
     current_dir = os.path.dirname(os.path.abspath(__file__))
-
-    yield
+    try:
+        yield
+    finally:
+        pass
 
 router = APIRouter(lifespan=router_lifespan)
+router.mount("/frontEnd/css", StaticFiles(directory="frontEnd/css"), name="static")
 
-@router.get("/qb-rankings", response_class = HTMLResponse)
+@router.get("/qb-rankings", response_class = JSONResponse)
 def get_qb_rankings():
     primary_qb_rankings = pd.read_csv("data/qb_rankings.csv")
     secondary_qb_rankings = pd.read_csv("data/secondary_qb_rankings.csv")
@@ -32,23 +36,10 @@ def get_qb_rankings():
     merged_qb_rankings = pd.merge(primary_qb_rankings, secondary_qb_rankings, on='QB')
     merged_qb_rankings['Mean'] = (merged_qb_rankings['Points_x'] + merged_qb_rankings['Points_y'])/2
     merged_qb_rankings = merged_qb_rankings.rename(columns = {
-        'Points_x': 'Method 1 Points',
-        'Points_y': 'Method 2 Points'
+        'Points_x': 'M1_Points',
+        'Points_y': 'M2_Points'
     })
     merged_qb_rankings = merged_qb_rankings.sort_values(by='Mean', ascending=False)
     merged_qb_rankings = merged_qb_rankings.drop(columns=['Unnamed: 0_x', 'Unnamed: 0_y'])
 
-    merged_table = merged_qb_rankings.to_html(index=False, classes="merged_qb_rankings", border=0)
-
-    return f"""
-    <!DOCTYPE HTML>
-    <html>
-        <head>
-            <title>QB Rankings</title>
-        </head>
-        <body>
-            <h1>QB Rankings</h1>
-            {merged_table}
-        </body>
-    </html>
-    """
+    return merged_qb_rankings.to_dict(orient="records")
