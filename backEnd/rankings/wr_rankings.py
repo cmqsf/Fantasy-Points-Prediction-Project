8f256ab8f5fcfd558cbb97449d9692d8daab0c2a
cmqsf <cmquarterman@gmail.com>
2025-08-25 22:01:05 -0700

    Create new rankings.py

---
+++ backEnd/rankings/wr_rankings.py
@@ -0,0 +1,42 @@
+
+import pandas as pd
+import numpy as np
+import random as rd
+import logging
+import math
+import os
+import logging
+import statistics
+
+import logging
+
+from fastapi import FastAPI, HTTPException, APIRouter, Query
+from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+
+logger = logging.getLogger(__name__)
+
+@asynccontextmanager
+async def router_lifespan(app: APIRouter):
+    global stats
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    try:
+        yield
+    finally:
+        pass
+
+router = APIRouter(lifespan=router_lifespan)
+
+@router.get("/wr-rankings", response_class=JSONResponse)
+def get_wr_rankings(): 
+    try: 
+        merged = pd.read_csv("data/merged_wr_rankings.csv")
+        return merged.to_dict(orient="records")
+
+    except Exception as e: 
+        logger.error(f"Error in get_wr_rankings: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
